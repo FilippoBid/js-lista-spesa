0fc1f0c2ad62f138c887b596187db2cc797f7a8e
FilippoBid <filippoperrone1997@gmail.com>
2022-11-07 14:15:07 +0100

    inizio

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+const ulEl = document.querySelector(".list-group");
+const shoppingitems = ["lemon", "gin", "vodka", "tonica", "ghiccio", "bicchieri"];
+let currentitemcount = 0;
+
+while { currentitemcount  shoppingitems.lenght }
+
+ 
