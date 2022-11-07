9602bfbf6ae66081cb34bd179f81029c92d916ba
FilippoBid <filippoperrone1997@gmail.com>
2022-11-07 18:29:12 +0100

    logica esercizio ci siamo ma non va

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,13 @@
-const ulEl = document.querySelector(".list-group");
+const ulEl = document.getElementById('mylist');
 const shoppingitems = ["lemon", "gin", "vodka", "tonica", "ghiccio", "bicchieri"];
+const liEl = document.createElement("li");
 let currentitemcount = 0;
 
-while { currentitemcount  shoppingitems.lenght }
+while ( currentitemcount < shoppingitems.lenght ) {
+    liEl.innerHTML = `${shoppingitems[currentitemcount]}`;
+    console.log("ciao");
+    currentitemcount++;
+    ulEl.append(liEl);
+};
 
- 
+
